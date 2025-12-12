f9394a5162d2167dd7565e93a64aa6293ce3cbee
Ayush Singh <ayushsingh02082004@gmail.com>
2025-12-12 16:23:49 +0530

    uc1 commit

---
+++ Snake and Ladder/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Snake_and_Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // this is uc 1
+            int start = 0;
+            int player1 = 0;
+
+            Console.WriteLine($"{start} and   {player1}");
+        }
+    }
+}
