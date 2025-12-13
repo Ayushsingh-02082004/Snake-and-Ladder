caf070e692f441c5fecee0881c02eb7138dec5d0
Ayush Singh <ayushsingh02082004@gmail.com>
2025-12-13 22:55:06 +0530

    commit of third use case

--- Snake and Ladder/Program.cs
+++ Snake and Ladder/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace Snake_and_Ladder
+﻿using System;
+using System.Numerics;
+using System.Threading;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Snake_and_Ladder
 {
     internal class Program
     {
@@ -9,13 +15,40 @@
             int start = 0;
             int player1 = 0;
 
-            Console.WriteLine($"{start} and   {player1}");
+            Console.WriteLine($"Currently start is : {start} and player is :  {player1}");
 
             //This is uc 2
             
             Random rnd = new Random();
             int dice = rnd.Next(1, 7);
             Console.WriteLine($"Number between 1 to 6 is : {dice}");
+
+            //This is uc 3
+            //The Player then checks for a Option. They are No Play,
+            //Ladder or Snake. - Use((RANDOM)) to check for Options - In Case of No Play the player stays in the same position
+            //In Case of Ladder the player moves ahead by the number of position received in the die
+            //In Case of Snake the player moves behind by the UC 3 number of position received in the die
+
+
+            int option = rnd.Next(1, 4);
+            int noplay = 0;
+            int ladder = 1;
+            int snake = 2;
+            
+            if(option == 1)
+            {
+                Console.WriteLine($"you got Ladderr now number currently is  : {start} and dice is : {dice} ");
+                start += dice;
+                Console.WriteLine($"NUmber after change is {start}");
+            }
+            else if(option == 2)
+            {
+                Console.WriteLine($"you Got ssnake now number currently is  : {start} and dice is : {dice} ");
+                start -= dice;
+                Console.WriteLine($"no after change is {start}");
+            }
+
+
         }
     }
 }
